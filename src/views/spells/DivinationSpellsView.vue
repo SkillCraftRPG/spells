fa96a434cb29bcd532fc792871774274a2b8e42e
Francis Pion <francispion@hotmail.com>
2026-09-26 11:30:04 -0400

    divination

--- src/views/spells/DivinationSpellsView.vue
+++ src/views/spells/DivinationSpellsView.vue
@@ -1,7 +1,8 @@
 <template>
   <main class="container-fluid page">
     <h1>{{ title }}</h1>
-    <SpellList school="Divination" />
+    <SpellList class="mb-3" school="Divination" />
+    <SpellDomain domain="divination" />
   </main>
 </template>
 
@@ -9,6 +10,7 @@
 import { computed, watchEffect } from "vue";
 import { useI18n } from "vue-i18n";
 
+import SpellDomain from "@/components/spells/SpellDomain.vue";
 import SpellList from "@/components/spells/SpellList.vue";
 import { useDocument } from "@/composables/document";
 
